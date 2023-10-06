adf7312a26d112fbe7756c61ff055d920aa30478
HuaKaiFuGuidoge <126623878+HuaKaiFuGuidoge@users.noreply.github.com>
2023-10-06 14:39:53 +0800

    Add files via upload

--- other/music/music.js
+++ other/music/music.js
@@ -1,7 +1,18 @@
 const audio = document.getElementById('music');
 const button = document.getElementById('music_button');
-let isRotating = false; 
+let isRotating = false;
+var musicUrls = [    
+  './other/music/雨的印记.mp3',    
+  './other/music/天空の城.mp3',   
+  './other/music/遠い空へ.mp3'  
+];  
 
+//选择音乐    
+var randomIndex = Math.floor(Math.random() * musicUrls.length);    
+var randomUrl = musicUrls[randomIndex];       
+audio.src = randomUrl; 
+
+//音乐播放
 button.addEventListener('click', () => {  
     if (audio.paused) {  
       audio.play();  
@@ -10,6 +21,7 @@ button.addEventListener('click', () => {
     }  
   });
 
+//音乐动画
 button.addEventListener("click", () => {    
   isRotating = !isRotating;    
   if (isRotating) {    
